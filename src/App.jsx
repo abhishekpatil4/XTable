10388704090a1a20a27df086a9753d390a47aaed
Abhishek Patil <abishkpatil@gmail.com>
2024-06-08 12:14:32 +0530

    feat: add feat to sort table contents using 'Sort by date' button

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,76 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+const info = [
+
+  { date: "2022-09-01", views: 100, article: "Article 1" },
+
+  { date: "2023-09-01", views: 100, article: "Article 1" },
+
+  { date: "2023-09-02", views: 150, article: "Article 2" },
+
+  { date: "2023-09-02", views: 120, article: "Article 3" },
+
+  { date: "2020-09-03", views: 200, article: "Article 4" }
+
+]
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState(info);
+  const sortByDate = () => {
+    const s = data.length;
+    const newArr = [...data];
+    for (let i = 0; i < s - 1; i++) {
+      for (let j = 0; j < s - i - 1; j++) {
+        //if dates are same
+        if (newArr[j].date === newArr[j + 1].date) {
+          if (newArr[j].views < newArr[j + 1].views) {
+            let temp = newArr[j];
+            newArr[j] = newArr[j + 1];
+            newArr[j + 1] = temp;
+          }
+        } else {
+          const d1 = new Date(newArr[j].date);
+          const d2 = new Date(newArr[j + 1].date);
+          if (d1 < d2) {
+            let temp = newArr[j];
+            newArr[j] = newArr[j + 1];
+            newArr[j + 1] = temp;
+          }
+        }
+      }
+    }
+    setData(newArr);
+  }
+  const sortByViews = () => {
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  }
+  return <div>
+    <h1>Date and Views Table</h1>
+    <button onClick={sortByDate}>Sort by Date</button>
+    <button onClick={sortByViews}>Sort by Views</button>
+    <table>
+      <thead>
+        <tr>
+          <th>Date</th>
+          <th>Views</th>
+          <th>Article</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          data.map((d, idx) =>
+            <tr key={idx}>
+              <td>{d.date}</td>
+              <td>{d.views}</td>
+              <td>{d.article}</td>
+            </tr>
+          )
+        }
+      </tbody>
+    </table>
+  </div>
 }
 
 export default App
